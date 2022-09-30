ddf3596f7dd1c3d5531f1f97ef2508b3488655de
Minhyeok Park <opensource@pmh.codes>
2022-09-30 13:50:58 +0900

    chore(pwa): add serviceworker

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,12 +9,22 @@ import QRScanPage from '../QRScanPage/QRScanPage'
 import UserPage from '../UserPage/UserPage'
 import BoothListPage from '../BoothListPage/BoothListPage'
 import BoothPage from '../BoothPage/BoothPage'
+import { useEffect } from 'react'
 
 const App = (): JSX.Element => {
   const [isLogined] = useCookie('SESSION_TOKEN')
   if (isLogined === null) {
     return <LoginPage />
   }
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch('/').catch(() => {
+        alert('인터넷 연결이 불안정합니다. WIFI및 데이터 네트워크 상태를 확인해 주세요.')
+        window.close()
+      })
+    }, 10 * 1000)
+  }, [])
 
   return (
     <BrowserRouter>

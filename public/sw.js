80be8077691de8569db4992552c2706f4d1ec4ce
Minhyeok Park <opensource@pmh.codes>
2022-10-01 13:46:01 +0900

    feat(pwa): add pwa sync

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,45 @@
-const staticOutgo = 'sowonpay'
-const assets = ['/']
+const FILES_TO_CACHE = [
+  '/offline.html'
+]
+const CACHE_NAME = 'sowonpay'
 
-self.addEventListener('install', installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticOutgo).then(cache => {
-      cache.addAll(assets)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE)
     })
   )
 })
 
-self.addEventListener('fetch', fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
+self.addEventListener('activate', event => {
+  const onActivate = async () => {
+
+  }
+
+  event.waitUntil(onActivate())
+})
+
+self.addEventListener('sync', function (event) {
+
+})
+
+self.addEventListener('periodicsync', async event => {
+  const onPeriodicSync = async () => {
+  }
+
+  event.waitUntil(onPeriodicSync())
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode !== 'navigate') { return }
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(async () => {
+        return caches.open(CACHE_NAME)
+          .then(cache => {
+            return cache.match('/offline.html')
+          })
+      })
   )
 })
